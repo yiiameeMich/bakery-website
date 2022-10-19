79d79e80cd64c7fc2f204c97586d1473be4fe6af
Michael <kuharskiy10@gmail.com>
2022-10-20 00:41:36 +0300

    js second commit

--- js/script.js
+++ js/script.js
@@ -63,6 +63,60 @@ aboutButtonsParent.addEventListener('click', (event) => {
 		}
 })
 
+const featuredCardsContainer = document.querySelector('.featured_cards-wrapper'),
+	featuredCards = document.querySelectorAll('.featured_card'),
+	hoverImage = document.querySelectorAll('.featured_hover_image'),
+	featuredCardIcon = document.querySelectorAll('.featured_card-icon')
+
+const makeCardsInactive = () => {
+	featuredCards.forEach(card => {
+		if (card.classList.contains('featured_card_active')) {
+			card.classList.remove('featured_card_active');
+			card.classList.add('featured_card');
+			card.classList.remove('fade');
+		}
+	})
+
+	hoverImage.forEach(image => {
+		if (image.classList.contains('hover-image-active')) {
+			image.classList.remove('hover-image-active');
+			image.classList.add('hover-image');
+			image.classList.remove('fade');
+		}
+	})
+
+	featuredCardIcon.forEach((icon,i) => {
+		icon.src = `images/icons/icon${i+1}.png`
+	})
+
+
+};
+
+const makeCardActive = (i = 0) => {
+	featuredCards[i].classList.remove('featured_card');
+	featuredCards[i].classList.add('featured_card_active');
+	hoverImage[i].classList.remove('hover-image');
+	hoverImage[i].classList.add('hover-image-active');
+	featuredCardIcon[i].src = `images/icons/icon${i+1}-alt.png`;
+	hoverImage[i].classList.add('fade');
+	featuredCards[i].classList.add('fade');
+};
+
+makeCardsInactive();
+makeCardActive();
+
+featuredCardsContainer.addEventListener('mouseover', (e) => {
+	const target = e.target;
+
+	if (target && target.classList.contains('f-card')){
+		featuredCards.forEach((card, i) => {
+			if (target === card) {
+				makeCardsInactive();
+				makeCardActive(i);
+			}
+		})
+	}
+});
 
 
 
